10421a3a14cac504ecb886686bc89cee46163cb3
Gabriel Harris-Rouquette <gabizou@me.com>
2020-08-20 17:59:13 -0700

    Refactor a bit of ChangeBlockEvent for better understanding of transactions
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/api/CatalogTypes.java
+++ src/main/java/org/spongepowered/api/CatalogTypes.java
@@ -31,6 +31,7 @@ import org.spongepowered.api.advancement.AdvancementType;
 import org.spongepowered.api.advancement.criteria.trigger.Trigger;
 import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.entity.BlockEntityType;
+import org.spongepowered.api.block.transaction.Operation;
 import org.spongepowered.api.command.selector.SelectorType;
 import org.spongepowered.api.data.persistence.DataFormat;
 import org.spongepowered.api.data.persistence.DataTranslator;
@@ -205,6 +206,8 @@ public final class CatalogTypes {
     public static final Class<NotePitch> NOTE_PITCH = NotePitch.class;
 
     public static final Class<ObjectiveDisplayMode> OBJECTIVE_DISPLAY_MODE = ObjectiveDisplayMode.class;
+
+    public static final Class<Operation> BLOCK_TRANSACTION_OPERATION = Operation.class;
 
     public static final Class<PandaGene> PANDA_GENE = PandaGene.class;
 
